4f757b942edfd8927d4b6b4acc3f79e4b726d7d2
Vitorbelfi <vitorabm2015@gmail.com>
2023-09-05 16:25:17 -0300

    Modelos e Telas Completo

--- src/App.js
+++
@@ -1,8 +0,0 @@
-function App(props) {
-
-  return (
-    <>
-    </>
-  );
-}
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
+import Login from './login';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/Login",
+    element: <Login/>
+  }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App nota="9"/>
-  </React.StrictMode>
+  <RouterProvider router = {router} />
 );
 
 
